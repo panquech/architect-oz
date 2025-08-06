3e0289b1686710b47bda118b060ef5e0b5e5908b
panquech <150560554+panquech@users.noreply.github.com>
2025-08-06 07:13:06 -0600

    Form components

---
+++ resources/views/components/forms/label.blade.php
@@ -0,0 +1,5 @@
+@props([
+    'name',
+    'label'
+])
+<label for="{{ $name }}" class="block text-sm/6 font-semibold text-gray-900">{{ $label }}</label>
---
+++ resources/views/components/forms/select.blade.php
@@ -0,0 +1,19 @@
+@props([
+    'name',
+    'label'
+])
+@php
+    $defaults = [
+        'id' => $name,
+        'name' => $name,
+        'class' => 'block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600',
+        'value' => old($name)
+    ];
+@endphp
+<x-forms.field :$label :$name>
+    <div class="mt-2.5">
+        <select {{ $attributes($defaults) }}>
+        {{ $slot }}
+        </select>
+    </div>
+</x-forms.field>
