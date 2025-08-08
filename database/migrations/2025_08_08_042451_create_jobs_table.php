5e248bb1a78895420ce2b8881b0270246dcf726f
panquech <150560554+panquech@users.noreply.github.com>
2025-08-08 00:55:45 -0600

    Changed the migrations' distribution and created the seeders

--- database/migrations/2025_08_08_042451_create_jobs_table.php
+++ database/migrations/2025_08_08_042451_create_jobs_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Company;
 
 return new class extends Migration
 {
@@ -13,11 +14,13 @@ return new class extends Migration
     {
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Company::class)->constrained()->cascadeOnDelete();
             $table->string('name');
-            $table->string('location');
+            $table->string('address');
             $table->string('period');
-            $table->string('position');
-            $table->string('work');
+            $table->string('position')->nullable();
+            $table->string('work')->nullable();
+            $table->string('url')->nullable(); 
             $table->timestamps();
         });
 
--- database/migrations/2025_08_08_042831_create_companies_table.php
+++ database/migrations/2025_08_08_042831_create_companies_table.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\Job;
-use App\Models\Company;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,13 +14,6 @@ return new class extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('url')->nullable(); 
-            $table->timestamps();
-        });
-        Schema::create('company_job', function (Blueprint $table) {
-            $table->id();
-            $table->foreignIdFor(Job::class)->constrained()->cascadeOnDelete();
-            $table->foreignIdFor(Company::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -33,6 +24,5 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('companies');
-        Schema::dropIfExists('company_job');
     }
 };
